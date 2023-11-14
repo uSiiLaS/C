813ebfde4409d96f67f3c0c650524926febb9bfa
uSiiLaS <142810322+uSiiLaS@users.noreply.github.com>
2023-11-14 08:13:08 -0300

    Add files via upload

---
+++ Atribuir valor 0 Vetor.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <time.h>
+#include <ctype.h>
+
+int main(){
+	
+	int valor[5];
+	int i;
+	
+	for(i=0; i<5; i++){
+		printf("Informe o %dº valor: ", i+1);
+		scanf("%d", &valor[i]);
+		
+		if(valor[i] < 0){
+			valor[i] = 0;
+		}
+	}
+	
+	system("cls || clear");
+
+printf("Valores do vetor\n");
+
+	for(i=0; i<5; i++){
+		printf("\t%d\n", valor[i]);
+	}
+	
+	return 0;
+}
